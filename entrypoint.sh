78ad50d09208d525bc35a7484dc9a229f9b406a3
Conanjoy <119912899+Conanjoy@users.noreply.github.com>
2023-02-23 23:56:46 +0530

    First Deploy

---
+++ entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+nohup gunicorn keep_alive:app --bind 0.0.0.0:8080 &
+
+python3 ms_rewards_farmer.py --session --headless --fast --telegram "${TOKEN}" "${CHAT_ID}" ;
+
+python3 ms_rewards_farmer.py --everyday 04:00 --session --headless --fast --telegram "${TOKEN}" "${CHAT_ID}"
+
